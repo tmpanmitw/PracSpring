07a6c68dcfebe9d35853be9c5187dc01e32266f0
tmpanmitw <112293743+tmpanmitw@users.noreply.github.com>
2022-11-15 00:22:41 +0900

    Add files via upload
    PracSpringSocketio
    - socketio 라이브러리를 이용한 간단한 채팅기능 구현

---
+++ PracSpringSocketio/src/main/java/com/example/pracspringsocketio/Message.java
@@ -0,0 +1,20 @@
+package com.example.pracspringsocketio;
+
+import lombok.Data;
+
+@Data
+public class Message {
+    private MessageType type;
+    private String message;
+    private String room;
+
+    public Message() {
+    }
+
+    public Message(MessageType type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+}
+
---
+++ PracSpringSocketio/src/main/java/com/example/pracspringsocketio/PracSpringSocketioApplication.java
@@ -0,0 +1,13 @@
+package com.example.pracspringsocketio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSpringSocketioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracSpringSocketioApplication.class, args);
+    }
+
+}
